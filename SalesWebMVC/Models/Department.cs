9c38e5813f44c38fc74158856c261d0b77b4c200
Otavio Paes <otaviopaesl@outlook.com>
2020-01-15 20:20:57 -0300

    First Model-View-Controller - Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+    }
+}
