3f2a5efac437c4b8b6906acea015466615f40f3d
Otavio Paes <otaviopaesl@outlook.com>
2020-01-16 20:34:26 -0300

    Other Entities and second migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMVC.Models;
 
+namespace SalesWebMVC.Models
+{
     public class SalesWebMVCContext : DbContext
     {
-        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+        public SalesWebMVCContext(DbContextOptions<SalesWebMVCContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
+}
--- SalesWebMVC/Migrations/20200116181939_Initial.Designer.cs
+++ SalesWebMVC/Migrations/20200116181939_Initial.Designer.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Migrations
 {
