f90032b25fe46900bddbf6b413d1c25cdd0346a5
Otavio Paes <otaviopaesl@outlook.com>
2020-01-16 14:57:39 -0300

    Included NuPackages and used CRUD Scaffolding to create DepartmentsController

---
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+    public class SalesWebMVCContext : DbContext
+    {
+        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+    }
