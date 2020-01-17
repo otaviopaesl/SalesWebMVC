23886aa2f0ca110e2b0723089b287b7e8ddfe29a
Otavio Paes <otaviopaesl@outlook.com>
2020-01-17 12:54:22 -0300

    foreign key not null (referential integrity and DateTime validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -11,11 +11,14 @@ namespace SalesWebMVC.Models
         public string Name { get; set; }
         public string Email { get; set; }
 
-        [DataType(DataType.DateTime)]
+        [Required]
+        [DataType(DataType.Date, ErrorMessage = "Date is out of Range")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
